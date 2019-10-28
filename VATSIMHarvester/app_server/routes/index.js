55b73a5ba5d929d92ad5c5012170bc253fb691c5
Joanna Summerlin <jksummerlin1@buffs.wtamu.edu>
2019-10-28 13:08:27 -0500

    Edited VATSIMHarvester

--- VATSIMHarvester/app_server/routes/index.js
+++ VATSIMHarvester/app_server/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+const client = [
+{
+  id: 100,
+  name: "Dude",
+  role: "Pilot"
+},
+{
+  id: 101,
+  name: "Dude2",
+  role: "Pilot"
+}
+]
+
+/*
+howMany
+filter
+offset
+*/
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/*GET a single client object */
+
+router.get('/api/one', function(reg, res, next){
+  // what do we do here?
+  res.send(`${client.name} is a ${client.role}`);
+});
+
+router.get('/api/json/one', function(reg, res, next){
+  // what do we do here?
+  res.json(client);
 });
 
 module.exports = router;
