2963cc331847ab33737491c16e7c375d82be5444
Joanna Summerlin <jksummerlin1@buffs.wtamu.edu>
2019-11-13 21:49:59 -0600

    Added folders

---
+++ Tour of Heros Tutorial/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void
+  {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void
+  {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+}
